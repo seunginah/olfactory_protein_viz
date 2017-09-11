8c1fe5b21d3598e2b04b84f2930f8699ae50fee9
jmenter-93 <jordanlmenter@gmail.com>
2017-09-11 08:52:25 -0400

    pseudocode on clusters branch

---
+++ generate_clusters.R
@@ -0,0 +1,11 @@
+# Load csv
+
+# Select zone, segment, protein expression column?, protein columns
+# Reshape data
+# Bin protein frequency features
+
+# Cluster with k means or hierarchical clustering, based on user input (number
+# of clusters)
+# Validate! do these clusters make sense? (iterate with Arie too)
+
+# How to "link" to our javascript visualization?
